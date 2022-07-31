40233b0282c70e48ef32893401827bb8651eca5b
Elysium Santata <camaleplata@outlook.com>
2022-07-31 02:15:57 -0300

    anima escroll refatorado com class

--- js/modules/scroll-animado.js
+++ js/modules/scroll-animado.js
@@ -2,24 +2,34 @@
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.tamanhoTela = window.innerHeight * 0.75;
-    this.animaScroll = this.animaScroll.bind(this);
+    this.tamanhoTela = innerHeight * 0.6;
+    this.checkDistance = this.checkDistance.bind(this);
   }
-
-  animaScroll(event) {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.tamanhoTela < 0;
-      if (isSectionVisible) {
-        section.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.tamanhoTela),
+      };
+    });
+  }
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
+
   init() {
-    this.animaScroll();
-    window.addEventListener("scroll", this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
     return this;
   }
 }
